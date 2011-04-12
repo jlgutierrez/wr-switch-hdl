b620e9974242824f7d5d068beb876c7121acde9a
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-04-12 01:12:52 +0000

    HDL: wrsw_swcore: removed modulo operator (unsynthesizable)

--- swc_pck_transfer_arbiter.vhd
+++ swc_pck_transfer_arbiter.vhd
@@ -6,7 +6,7 @@
 -- Author     : Maciej Lipinski
 -- Company    : CERN BE-Co-HT
 -- Created    : 2010-11-03
--- Last update: 2010-11-03
+-- Last update: 2011-03-25
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -57,72 +57,80 @@ entity swc_pck_transfer_arbiter is
 -- I/F with output block
 -------------------------------------------------------------------------------
 
-    ob_data_valid_o : out  std_logic_vector(c_swc_num_ports -1 downto 0);
-    
-    ob_ack_i : in  std_logic_vector(c_swc_num_ports -1 downto 0);
+    ob_data_valid_o : out std_logic_vector(c_swc_num_ports -1 downto 0);
+    ob_ack_i        : in  std_logic_vector(c_swc_num_ports -1 downto 0);
+    ob_pageaddr_o   : out std_logic_vector(c_swc_num_ports * c_swc_page_addr_width - 1 downto 0);
+    ob_prio_o       : out std_logic_vector(c_swc_num_ports * c_swc_prio_width - 1 downto 0);
+    ob_pck_size_o   : out std_logic_vector(c_swc_num_ports * c_swc_max_pck_size_width - 1 downto 0);
 
-    ob_pageaddr_o : out  std_logic_vector(c_swc_num_ports * c_swc_page_addr_width - 1 downto 0);
-    
-    ob_prio_o     : out  std_logic_vector(c_swc_num_ports * c_swc_prio_width - 1 downto 0);
-
-    ob_pck_size_o : out  std_logic_vector(c_swc_num_ports * c_swc_max_pck_size_width - 1 downto 0);
 -------------------------------------------------------------------------------
 -- I/F with Input Block
 -------------------------------------------------------------------------------     
     ib_transfer_pck_i : in  std_logic_vector(c_swc_num_ports -1 downto 0);
-    
-    ib_transfer_ack_o : out  std_logic_vector(c_swc_num_ports -1 downto 0);
-    
-    ib_busy_o     : out  std_logic_vector(c_swc_num_ports  - 1 downto 0);  
-    
-    ib_pck_size_i : in  std_logic_vector(c_swc_num_ports * c_swc_max_pck_size_width - 1 downto 0);
-    
-    ib_pageaddr_i : in  std_logic_vector(c_swc_num_ports * c_swc_page_addr_width - 1 downto 0);
-    
-    ib_mask_i     : in  std_logic_vector(c_swc_num_ports * c_swc_num_ports - 1 downto 0);
+    ib_transfer_ack_o : out std_logic_vector(c_swc_num_ports -1 downto 0);
+    ib_busy_o         : out std_logic_vector(c_swc_num_ports - 1 downto 0);
 
-    ib_prio_i     : in  std_logic_vector(c_swc_num_ports * c_swc_prio_width - 1 downto 0)
-    
+    ib_pck_size_i : in std_logic_vector(c_swc_num_ports * c_swc_max_pck_size_width - 1 downto 0);
+
+    ib_pageaddr_i : in std_logic_vector(c_swc_num_ports * c_swc_page_addr_width - 1 downto 0);
+
+    ib_mask_i : in std_logic_vector(c_swc_num_ports * c_swc_num_ports - 1 downto 0);
+
+    ib_prio_i : in std_logic_vector(c_swc_num_ports * c_swc_prio_width - 1 downto 0)
+
     );
 end swc_pck_transfer_arbiter;
-    
-architecture syn of swc_pck_transfer_arbiter is    
+
+architecture syn of swc_pck_transfer_arbiter is
+
+  function f_modulo_numports(x : integer) return integer is
+  begin
+    if(x < c_swc_num_ports) then
+      return x;
+    elsif(x < 2*c_swc_num_ports) then
+      return (x-c_swc_num_ports);
+    elsif(x < 3 * c_swc_num_ports) then
+      return (x-2*c_swc_num_ports);
+    else
+      return 0;
+    end if;
+  end function;
 
 
-     subtype t_pageaddr     is std_logic_vector(c_swc_page_addr_width    - 1 downto 0);
-     subtype t_prio         is std_logic_vector(c_swc_prio_width         - 1 downto 0);
-     subtype t_mask         is std_logic_vector(c_swc_num_ports          - 1 downto 0);
-     subtype t_pck_size     is std_logic_vector(c_swc_max_pck_size_width - 1 downto 0);
-     
-     type t_pageaddr_array  is array (c_swc_num_ports - 1 downto 0) of t_pageaddr;
-     type t_prio_array      is array (c_swc_num_ports - 1 downto 0) of t_prio;
-     type t_mask_array      is array (c_swc_num_ports - 1 downto 0) of t_mask;
-     type t_pck_size_array  is array (c_swc_num_ports - 1 downto 0) of t_pck_size;
-     
-     ---------------------------------------------------------------------------
-     -- signals outputed from Pck Transfer Input (PTI)
-     -- before MUX !!!!
-     ---------------------------------------------------------------------------
-     signal pto_pageaddr      : t_pageaddr_array;
-     signal pto_output_mask   : t_mask_array;
-     signal pto_read_mask     : t_mask_array;
-     signal pto_prio          : t_prio_array;
-     signal pto_pck_size      : t_pck_size_array;
-     
-     ---------------------------------------------------------------------------
-     -- signals inputed to Pck Transfer Output (PTO) from Pck Transfer Input (TPI)
-     -- MUXED !!!!!!!!!!
-     ---------------------------------------------------------------------------     
-     signal pti_transfer_data_valid : std_logic_vector(c_swc_num_ports  - 1 downto 0);
-     signal pti_transfer_data_ack   : std_logic_vector(c_swc_num_ports  - 1 downto 0);
-     signal pti_pageaddr            : t_pageaddr_array;
-     signal pti_prio                : t_prio_array;
-     signal pti_pck_size      : t_pck_size_array;     
-     signal sync_sreg    : std_logic_vector(c_swc_num_ports - 1 downto 0);
-     signal sync_cntr    : integer range 0 to c_swc_num_ports - 1;
-     signal sync_cntr_ack : integer range 0 to c_swc_num_ports-1;
-     
-begin --arch
+  subtype t_pageaddr is std_logic_vector(c_swc_page_addr_width - 1 downto 0);
+  subtype t_prio is std_logic_vector(c_swc_prio_width - 1 downto 0);
+  subtype t_mask is std_logic_vector(c_swc_num_ports - 1 downto 0);
+  subtype t_pck_size is std_logic_vector(c_swc_max_pck_size_width - 1 downto 0);
+
+  type t_pageaddr_array is array (c_swc_num_ports - 1 downto 0) of t_pageaddr;
+  type t_prio_array is array (c_swc_num_ports - 1 downto 0) of t_prio;
+  type t_mask_array is array (c_swc_num_ports - 1 downto 0) of t_mask;
+  type t_pck_size_array is array (c_swc_num_ports - 1 downto 0) of t_pck_size;
+
+---------------------------------------------------------------------------
+-- signals outputed from Pck Transfer Input (PTI)
+-- before MUX !!!!
+---------------------------------------------------------------------------
+  signal pto_pageaddr    : t_pageaddr_array;
+  signal pto_output_mask : t_mask_array;
+  signal pto_read_mask   : t_mask_array;
+  signal pto_prio        : t_prio_array;
+  signal pto_pck_size    : t_pck_size_array;
+
+---------------------------------------------------------------------------
+-- signals inputed to Pck Transfer Output (PTO) from Pck Transfer Input (TPI)
+-- MUXED !!!!!!!!!!
+---------------------------------------------------------------------------     
+  signal pti_transfer_data_valid : std_logic_vector(c_swc_num_ports - 1 downto 0);
+  signal pti_transfer_data_ack   : std_logic_vector(c_swc_num_ports - 1 downto 0);
+  signal pti_pageaddr            : t_pageaddr_array;
+  signal pti_prio                : t_prio_array;
+  signal pti_pck_size            : t_pck_size_array;
+  signal sync_sreg               : std_logic_vector(c_swc_num_ports - 1 downto 0);
+  signal sync_cntr               : integer range 0 to c_swc_num_ports - 1;
+  signal sync_cntr_ack           : integer range 0 to c_swc_num_ports-1;
+
+begin  --arch
 
 
   
@@ -133,8 +141,8 @@ begin --arch
       if(rst_n_i = '0') then
         sync_sreg (0)                             <= '1';
         sync_sreg (sync_sreg'length - 1 downto 1) <= (others => '0');
-        sync_cntr                                 <= 0; --c_swc_num_ports - 1;
-        sync_cntr_ack                             <= c_swc_num_ports - 1; --c_swc_num_ports - 2; -- c_swc_packet_mem_multiply-1;
+        sync_cntr                                 <= 0;  --c_swc_num_ports - 1;
+        sync_cntr_ack                             <= c_swc_num_ports - 1;  --c_swc_num_ports - 2; -- c_swc_packet_mem_multiply-1;
 
       else
         sync_sreg <= sync_sreg(sync_sreg'length-2 downto 0) & sync_sreg(sync_sreg'length-1);
@@ -160,94 +168,94 @@ begin --arch
 
   -- multiplex mask from input to output
   --multimux_out : process(sync_cntr,pto_output_mask,pto_pageaddr,pto_prio)
-  multimux_out : process(sync_cntr,pto_output_mask,pto_pageaddr,pto_prio,pto_pck_size)
-  begin 
-  
-     for i in 0 to c_swc_num_ports - 1 loop  
-       pti_transfer_data_valid(i) <= pto_output_mask((sync_cntr + i) mod c_swc_num_ports)(i);
-       pti_pageaddr           (i) <= pto_pageaddr   ((sync_cntr + i) mod c_swc_num_ports);
-       pti_prio               (i) <= pto_prio       ((sync_cntr + i) mod c_swc_num_ports);
-       pti_pck_size           (i) <= pto_pck_size   ((sync_cntr + i) mod c_swc_num_ports);
-     end loop;
-        
+  multimux_out : process(sync_cntr, pto_output_mask, pto_pageaddr, pto_prio, pto_pck_size)
+  begin
+    
+    for i in 0 to c_swc_num_ports - 1 loop
+      pti_transfer_data_valid(i) <= pto_output_mask(f_modulo_numports(sync_cntr + i))(i);
+      pti_pageaddr (i)           <= pto_pageaddr (f_modulo_numports(sync_cntr + i));
+      pti_prio (i)               <= pto_prio (f_modulo_numports(sync_cntr + i));
+      pti_pck_size (i)           <= pto_pck_size (f_modulo_numports(sync_cntr + i));
+    end loop;
+    
   end process;
 
-      
+
   -- we get ack from output ports and translate it into masks' bits in input ports
   multidemux_in : process (clk_i, rst_n_i)
   begin
-     if rising_edge(clk_i) then
-       if(rst_n_i = '0') then
-       
-           for i in 0 to c_swc_num_ports -1 loop
-               pto_read_mask(i) <= (others =>'0');  
-           end loop;
-       
-       else
-
-
-         for i in 0 to c_swc_num_ports-1 loop   
-           
-           if(i = 1) then
-             pto_read_mask((sync_cntr_ack + i)  mod c_swc_num_ports)((i - 1))                            <= '0';  
-           elsif(i > 1) then
-             pto_read_mask((sync_cntr_ack + i)  mod c_swc_num_ports)((i - 1) downto 0)                   <= (others => '0');
-           end if;
-           
-           pto_read_mask((sync_cntr_ack + i)  mod c_swc_num_ports)( i)                                   <= pti_transfer_data_ack(i);  
-           
-           if(i < c_swc_num_ports - 2) then
-             pto_read_mask((sync_cntr_ack + i)  mod c_swc_num_ports)(c_swc_num_ports - 1 downto (i + 1)) <= (others => '0'); 
-           elsif(i = c_swc_num_ports - 2) then
-             pto_read_mask((sync_cntr_ack + i)  mod c_swc_num_ports)((i + 1))                            <=  '0';
-           end if;
-            
-         end loop;
+    if rising_edge(clk_i) then
+      if(rst_n_i = '0') then
         
-      end if;    
+        for i in 0 to c_swc_num_ports -1 loop
+          pto_read_mask(i) <= (others => '0');
+        end loop;
+        
+      else
+
+
+        for i in 0 to c_swc_num_ports-1 loop
+          
+          if(i = 1) then
+            pto_read_mask(f_modulo_numports(sync_cntr_ack + i))((i - 1)) <= '0';
+          elsif(i > 1) then
+            pto_read_mask(f_modulo_numports(sync_cntr_ack + i))((i - 1) downto 0) <= (others => '0');
+          end if;
+
+          pto_read_mask(f_modulo_numports(sync_cntr_ack + i))(i) <= pti_transfer_data_ack(i);
+
+          if(i < c_swc_num_ports - 2) then
+            pto_read_mask(f_modulo_numports(sync_cntr_ack + i))(c_swc_num_ports - 1 downto (i + 1)) <= (others => '0');
+          elsif(i = c_swc_num_ports - 2) then
+            pto_read_mask(f_modulo_numports(sync_cntr_ack + i))((i + 1)) <= '0';
+          end if;
+          
+        end loop;
+        
+      end if;
     end if;
-         
-   end process;  
-   
+    
+  end process;
+
   gen_input : for i in 0 to c_swc_num_ports-1 generate
-    TRANSFER_INPUT : swc_pck_transfer_input 
-    port map (
-      clk_i               => clk_i,
-      rst_n_i             => rst_n_i,
-      pto_transfer_pck_o  => open,
-      pto_pageaddr_o      => pto_pageaddr      (i),
-      pto_output_mask_o   => pto_output_mask   (i),
-      pto_read_mask_i     => pto_read_mask     (i),
-      pto_prio_o          => pto_prio          (i),
-      pto_pck_size_o      => pto_pck_size      (i),
-      ib_transfer_pck_i   => ib_transfer_pck_i (i),
-      ib_pageaddr_i       => ib_pageaddr_i    ((i + 1)*c_swc_page_addr_width    - 1 downto i*c_swc_page_addr_width),
-      ib_mask_i           => ib_mask_i        ((i + 1)*c_swc_num_ports          - 1 downto i*c_swc_num_ports),
-      ib_prio_i           => ib_prio_i        ((i + 1)*c_swc_prio_width         - 1 downto i*c_swc_prio_width),
-      ib_pck_size_i       => ib_pck_size_i    ((i + 1)*c_swc_max_pck_size_width - 1 downto i*c_swc_max_pck_size_width),
-      ib_transfer_ack_o   => ib_transfer_ack_o (i),
-      ib_busy_o           => ib_busy_o         (i)
-      
-      );
+    TRANSFER_INPUT : swc_pck_transfer_input
+      port map (
+        clk_i              => clk_i,
+        rst_n_i            => rst_n_i,
+        pto_transfer_pck_o => open,
+        pto_pageaddr_o     => pto_pageaddr (i),
+        pto_output_mask_o  => pto_output_mask (i),
+        pto_read_mask_i    => pto_read_mask (i),
+        pto_prio_o         => pto_prio (i),
+        pto_pck_size_o     => pto_pck_size (i),
+        ib_transfer_pck_i  => ib_transfer_pck_i (i),
+        ib_pageaddr_i      => ib_pageaddr_i ((i + 1)*c_swc_page_addr_width - 1 downto i*c_swc_page_addr_width),
+        ib_mask_i          => ib_mask_i ((i + 1)*c_swc_num_ports - 1 downto i*c_swc_num_ports),
+        ib_prio_i          => ib_prio_i ((i + 1)*c_swc_prio_width - 1 downto i*c_swc_prio_width),
+        ib_pck_size_i      => ib_pck_size_i ((i + 1)*c_swc_max_pck_size_width - 1 downto i*c_swc_max_pck_size_width),
+        ib_transfer_ack_o  => ib_transfer_ack_o (i),
+        ib_busy_o          => ib_busy_o (i)
+
+        );
   end generate gen_input;
-  
+
   gen_output : for i in 0 to c_swc_num_ports-1 generate
     TRANSFER_OUTPUT : swc_pck_transfer_output
-    port map(
-      clk_i                    => clk_i,
-      rst_n_i                  => rst_n_i,
-      ob_transfer_data_valid_o => ob_data_valid_o        (i),
-      ob_pageaddr_o            => ob_pageaddr_o         ((i + 1)*c_swc_page_addr_width    - 1 downto i*c_swc_page_addr_width),
-      ob_prio_o                => ob_prio_o             ((i + 1)*c_swc_prio_width         - 1 downto i*c_swc_prio_width),
-      ob_pck_size_o            => ob_pck_size_o         ((i + 1)*c_swc_max_pck_size_width - 1 downto i*c_swc_max_pck_size_width),
-      ob_transfer_data_ack_i   => ob_ack_i               (i),
-      pti_transfer_data_valid_i=> pti_transfer_data_valid(i),
-      pti_transfer_data_ack_o  => pti_transfer_data_ack  (i),
-      pti_pageaddr_i           => pti_pageaddr           (i),
-      pti_prio_i               => pti_prio               (i),
-      pti_pck_size_i           => pti_pck_size           (i)
-      
-      );
+      port map(
+        clk_i                     => clk_i,
+        rst_n_i                   => rst_n_i,
+        ob_transfer_data_valid_o  => ob_data_valid_o (i),
+        ob_pageaddr_o             => ob_pageaddr_o ((i + 1)*c_swc_page_addr_width - 1 downto i*c_swc_page_addr_width),
+        ob_prio_o                 => ob_prio_o ((i + 1)*c_swc_prio_width - 1 downto i*c_swc_prio_width),
+        ob_pck_size_o             => ob_pck_size_o ((i + 1)*c_swc_max_pck_size_width - 1 downto i*c_swc_max_pck_size_width),
+        ob_transfer_data_ack_i    => ob_ack_i (i),
+        pti_transfer_data_valid_i => pti_transfer_data_valid(i),
+        pti_transfer_data_ack_o   => pti_transfer_data_ack (i),
+        pti_pageaddr_i            => pti_pageaddr (i),
+        pti_prio_i                => pti_prio (i),
+        pti_pck_size_i            => pti_pck_size (i)
+
+        );
   end generate gen_output;
   
-end syn; -- arch
+end syn;  -- arch
