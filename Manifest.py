483309896aaa3c84fcacd463ebdb186bbc5279a6
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-03-08 13:35:50 +0100

    added softpll-ng to the root manifest

--- Manifest.py
+++ Manifest.py
@@ -1,10 +1,16 @@
-modules = { "local" : [
-				"modules/wrsw_nic",
-				"modules/wrsw_rt_subsystem",
-				"modules/wrsw_txtsu",
-				"platform/virtex6/chipscope",
-				"modules/softpll-new" ],
- 					"git" : [ "git://ohwr.org/hdl-core-lib/wr-cores.git::wishbonized" ]
-};
-				
-				
+modules = { 
+    "local" : [
+        "modules/wrsw_nic",
+        "modules/wrsw_rt_subsystem",
+        "modules/wrsw_txtsu",
+        "modules/wrsw_rtu",
+        "platform/virtex6/chipscope",
+        "modules/softpll-new",
+        "modules/wrsw_swcore/maciek-native" 
+        ],
+    
+    "git" : [ "git://ohwr.org/hdl-core-lib/wr-cores.git::wishbonized" ]
+    };
+
+
+files = ["modules/wrsw_shared_types_pkg.vhd"];
