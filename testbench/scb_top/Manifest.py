5649a5362e9ea85e827e49b6be641e20f3845296
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-03-08 13:46:05 +0100

    testbench/scb_top: updated mainfest to point to correct top module

--- testbench/scb_top/Manifest.py
+++ testbench/scb_top/Manifest.py
@@ -6,7 +6,7 @@ vlog_opt = "+incdir+../../sim +incdir+../../sim/wr-hdl"
 
 files = [ "main.sv" ]
 
-modules = { "local" : ["../../", "../../top/scb_test"] }
+modules = { "local" : ["../../", "../../top/bare_top"] }
 					
 
 					
