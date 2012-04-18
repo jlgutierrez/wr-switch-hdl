c9e3276b59b20f81d7ccb324a2b6cb8031d8ddd9
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-04-18 15:59:49 +0200

    rt/include/stdint.h: added (u)int64_t types

--- rt/include/stdint.h
+++ rt/include/stdint.h
@@ -9,8 +9,11 @@
 typedef unsigned char	uint8_t;
 typedef unsigned short	uint16_t;
 typedef unsigned long	uint32_t;
+typedef unsigned long	long uint64_t;
+
 typedef signed char	int8_t;
 typedef signed short	int16_t;
 typedef signed long	int32_t;
+typedef signed long	long int64_t;
 
 #endif /* __ARCH_LM32_STDINT_H__ */
