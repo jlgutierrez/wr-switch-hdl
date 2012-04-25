c3bb62fed7c75df7fe3c07c1e3853ee693b02856
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-04-25 16:48:54 +0200

    root manifest: removed reference to softpll-new (moved to wr-cores)

--- Manifest.py
+++ Manifest.py
@@ -5,7 +5,6 @@ modules = { "local" : [
 				"modules/wrsw_swcore",
 				"modules/wrsw_rtu",
 				"platform/virtex6/chipscope",
-				"platform/xilinx",
-				"modules/softpll-new" ],
+				"platform/xilinx"],
  					"git" : [ "git://ohwr.org/hdl-core-lib/wr-cores.git::wishbonized" ]
 };
