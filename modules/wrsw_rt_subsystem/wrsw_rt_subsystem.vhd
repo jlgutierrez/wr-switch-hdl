8d0f9af7240425f0812f2ad6c84d7e61c253cdc4
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-04-25 16:37:46 +0200

    modules/wrsw_rt_subsystem: updated softpll with grandmaster mode, PPSgen instantiation inside RT subsys

--- modules/wrsw_rt_subsystem/wrsw_rt_subsystem.vhd
+++ modules/wrsw_rt_subsystem/wrsw_rt_subsystem.vhd
@@ -11,14 +11,15 @@ use work.wishbone_pkg.all;
 entity wrsw_rt_subsystem is
   
   generic (
-    g_num_rx_clocks : integer);
+    g_num_rx_clocks : integer;
+    g_simulation    : boolean);
 
   port(
     clk_ref_i  : in std_logic;
     clk_sys_i  : in std_logic;
     clk_dmtd_i : in std_logic;
     clk_rx_i   : in std_logic_vector(g_num_rx_clocks-1 downto 0);
-    clk_aux_i  : in std_logic;
+    clk_ext_i  : in std_logic;
 
     rst_n_i : in  std_logic;
     rst_n_o : out std_logic;
@@ -26,7 +27,10 @@ entity wrsw_rt_subsystem is
     wb_i : in  t_wishbone_slave_in;
     wb_o : out t_wishbone_slave_out;
 
-    -- DAC Drive
+    ---------------------------------------------------------------------------
+    -- PLL DAC Drive
+    ---------------------------------------------------------------------------
+
     dac_helper_sync_n_o : out std_logic;
     dac_helper_sclk_o   : out std_logic;
     dac_helper_data_o   : out std_logic;
@@ -39,10 +43,20 @@ entity wrsw_rt_subsystem is
     uart_txd_o : out std_logic;
     uart_rxd_i : in  std_logic;
 
-    pps_p_o : out std_logic;            -- cleaned-up, retimed PPS for use in
-                                        -- the rest of the switch
+    -------------------------------------------------------------------------------
+    -- Time base signals
+    -------------------------------------------------------------------------------    
 
-    pps_raw_i : in std_logic;           -- raw PPS input (from the front panel)
+    -- cleaned-up, retimed PPS for use in the rest of the switch
+    pps_csync_o : out std_logic;
+
+    -- PPS valid flag - when 1, pps_csync_o has produced at least two correctly
+    -- aligned PPS resync pulses (so we can be sure that endpoint's individual
+    -- TSCs are in sync with the master time counter and the timestamps are correct).
+    pps_valid_o : out std_logic;
+
+    pps_ext_i : in  std_logic;  -- external PPS input (from the front panel)
+    pps_ext_o : out std_logic;  -- external PPS output (to the front panel)
 
     sel_clk_sys_o : out std_logic;      -- system clock selection: 0 = startup
                                         -- clock, 1 = PLL clock
@@ -62,20 +76,27 @@ architecture rtl of wrsw_rt_subsystem is
 
   component xwr_softpll_ng
     generic (
-      g_tag_bits                 : integer;
-      g_interface_mode           : t_wishbone_interface_mode;
-      g_address_granularity      : t_wishbone_address_granularity;
-      g_num_ref_inputs           : integer;
-      g_num_outputs              : integer;
-     	g_with_debug_fifo: boolean := true
-
-      );
+      g_tag_bits             : integer;
+      g_num_ref_inputs       : integer;
+      g_num_outputs          : integer;
+      g_with_period_detector : boolean;
+      g_with_debug_fifo      : boolean;
+      g_with_ext_clock_input : boolean;
+      g_with_undersampling   : boolean;
+      g_reverse_dmtds        : boolean;
+      g_bb_ref_divider       : integer;
+      g_bb_feedback_divider  : integer;
+      g_bb_log2_gating       : integer;
+      g_interface_mode       : t_wishbone_interface_mode;
+      g_address_granularity  : t_wishbone_address_granularity);
     port (
       clk_sys_i       : in  std_logic;
       rst_n_i         : in  std_logic;
       clk_ref_i       : in  std_logic_vector(g_num_ref_inputs-1 downto 0);
       clk_fb_i        : in  std_logic_vector(g_num_outputs-1 downto 0);
       clk_dmtd_i      : in  std_logic;
+      clk_ext_i       : in  std_logic;
+      sync_p_i        : in  std_logic;
       dac_dmtd_data_o : out std_logic_vector(15 downto 0);
       dac_dmtd_load_o : out std_logic;
       dac_out_data_o  : out std_logic_vector(15 downto 0);
@@ -85,15 +106,77 @@ architecture rtl of wrsw_rt_subsystem is
       out_locked_o    : out std_logic_vector(g_num_outputs-1 downto 0);
       slave_i         : in  t_wishbone_slave_in;
       slave_o         : out t_wishbone_slave_out;
-      debug_o         : out std_logic_vector(3 downto 0));
+      debug_o         : out std_logic_vector(3 downto 0);
+      dbg_fifo_irq_o  : out std_logic);
   end component;
 
+  component xwr_pps_gen
+    generic (
+      g_interface_mode       : t_wishbone_interface_mode;
+      g_address_granularity  : t_wishbone_address_granularity;
+      g_ref_clock_rate       : integer;
+      g_ext_clock_rate       : integer;
+      g_with_ext_clock_input : boolean);
+    port (
+      clk_ref_i       : in  std_logic;
+      clk_sys_i       : in  std_logic;
+      clk_ext_i       : in  std_logic;
+      rst_n_i         : in  std_logic;
+      slave_i         : in  t_wishbone_slave_in;
+      slave_o         : out t_wishbone_slave_out;
+      pps_in_i        : in  std_logic;
+      pps_csync_o     : out std_logic;
+      pps_out_o       : out std_logic;
+      pps_valid_o     : out std_logic;
+      tm_utc_o        : out std_logic_vector(39 downto 0);
+      tm_cycles_o     : out std_logic_vector(27 downto 0);
+      tm_time_valid_o : out std_logic);
+  end component;
+
+
+
+-- interconnect layout:
+-- 0x00000 - 0x10000: RAM
+-- 0x10000 - 0x10100: UART
+-- 0x10100 - 0x10200: SoftPLL
+-- 0x10200 - 0x10300: SPI master (to PLL)
+-- 0x10300 - 0x10400: GPIO
+-- 0x10400 - 0x10500: Timer
+
   constant c_NUM_GPIO_PINS : integer := 32;
+  constant c_NUM_WB_SLAVES : integer := 7;
+
+  constant c_SLAVE_DPRAM   : integer := 0;
+  constant c_SLAVE_UART    : integer := 1;
+  constant c_SLAVE_SOFTPLL : integer := 2;
+  constant c_SLAVE_SPI     : integer := 3;
+  constant c_SLAVE_GPIO    : integer := 4;
+  constant c_SLAVE_TIMER   : integer := 5;
+  constant c_SLAVE_PPSGEN  : integer := 6;
+
+  constant c_cnx_base_addr : t_wishbone_address_array(c_NUM_WB_SLAVES-1 downto 0) :=
+    (x"00010500",
+     x"00010400",
+     x"00010300",
+     x"00010200",
+     x"00010100",
+     x"00010000",
+     x"00000000");
+
+  constant c_cnx_base_mask : t_wishbone_address_array(c_NUM_WB_SLAVES-1 downto 0) :=
+    (x"000fff00",
+     x"000fff00",
+     x"000fff00",
+     x"000fff00",
+     x"000fff00",
+     x"000fff00",
+     x"000f0000");
+
 
   signal cnx_slave_in   : t_wishbone_slave_in_array(1 downto 0);
   signal cnx_slave_out  : t_wishbone_slave_out_array(1 downto 0);
-  signal cnx_master_in  : t_wishbone_master_in_array(5 downto 0);
-  signal cnx_master_out : t_wishbone_master_out_array(5 downto 0);
+  signal cnx_master_in  : t_wishbone_master_in_array(c_NUM_WB_SLAVES-1 downto 0);
+  signal cnx_master_out : t_wishbone_master_out_array(c_NUM_WB_SLAVES-1 downto 0);
 
   signal cpu_iwb_out : t_wishbone_master_out;
   signal cpu_iwb_in  : t_wishbone_master_in;
@@ -107,37 +190,25 @@ architecture rtl of wrsw_rt_subsystem is
   signal dac_out_data, dac_dmtd_data : std_logic_vector(15 downto 0);
   signal dac_out_load, dac_dmtd_load : std_logic;
 
+  signal clk_rx_vec : std_logic_vector(g_num_rx_clocks-1 downto 0);
 
-  constant c_cnx_base_addr : t_wishbone_address_array(5 downto 0) :=
-    (x"00010400", x"00010300", x"00010200", x"00010100", x"00010000", x"00000000");
-
-  constant c_cnx_base_mask : t_wishbone_address_array(5 downto 0) :=
-    (x"000fff00", x"000fff00", x"000fff00", x"000fff00", x"000fff00", x"000f0000");
-
-  alias clk_ref_10m is pll_status_i;
-  signal clk_rx_vec : std_logic_vector(g_num_rx_clocks+1 downto 0);
-
-  function f_mask_single_bit(index : integer; size : integer) return std_logic_vector is
-    variable tmp : std_logic_vector(size-1 downto 0);
+  function f_pick (
+    cond     : boolean;
+    if_true  : integer;
+    if_false : integer
+    ) return integer is
   begin
-    tmp        := (others => '1');
-    tmp(index) := '0';
-    return tmp;
-  end f_mask_single_bit;
+    if(cond) then
+      return if_true;
+    else
+      return if_false;
+    end if;
+  end f_pick;
+
 
 begin  -- rtl
 
   clk_rx_vec(g_num_rx_clocks-1 downto 0) <= clk_rx_i;
-  clk_rx_vec(g_num_rx_clocks)   <= clk_ref_10m;
-  clk_rx_vec(g_num_rx_clocks+1) <= clk_aux_i;
-
--- interconnect layout:
--- 0x00000 - 0x10000: RAM
--- 0x10000 - 0x10100: UART
--- 0x10100 - 0x10200: SoftPLL
--- 0x10200 - 0x10300: SPI master (to PLL)
--- 0x10300 - 0x10400: GPIO
--- 0x10400 - 0x10500: Timer
 
   cnx_slave_in(0) <= wb_i;
   wb_o            <= cnx_slave_out(0);
@@ -145,21 +216,21 @@ begin  -- rtl
   U_Intercon : xwb_crossbar
     generic map (
       g_num_masters => 2,
-      g_num_slaves  => 6,
-      g_registered  => true)
+      g_num_slaves  => c_NUM_WB_SLAVES,
+      g_registered  => true,
+      g_address     => c_cnx_base_addr,
+      g_mask        => c_cnx_base_mask)
     port map (
-      clk_sys_i     => clk_sys_i,
-      rst_n_i       => rst_n_i,
-      slave_i       => cnx_slave_in,
-      slave_o       => cnx_slave_out,
-      master_i      => cnx_master_in,
-      master_o      => cnx_master_out,
-      cfg_address_i => c_cnx_base_addr,
-      cfg_mask_i    => c_cnx_base_mask);
+      clk_sys_i => clk_sys_i,
+      rst_n_i   => rst_n_i,
+      slave_i   => cnx_slave_in,
+      slave_o   => cnx_slave_out,
+      master_i  => cnx_master_in,
+      master_o  => cnx_master_out);
 
   U_CPU : xwb_lm32
     generic map (
-      g_profile => "medium")
+      g_profile => "medium_icache")
     port map (
       clk_sys_i => clk_sys_i,
       rst_n_i   => cpu_reset_n,
@@ -180,8 +251,8 @@ begin  -- rtl
     port map (
       clk_sys_i => clk_sys_i,
       rst_n_i   => rst_n_i,
-      slave1_i  => cnx_master_out(0),
-      slave1_o  => cnx_master_in(0),
+      slave1_i  => cnx_master_out(c_SLAVE_DPRAM),
+      slave1_o  => cnx_master_in(c_SLAVE_DPRAM),
       slave2_i  => cpu_iwb_out,
       slave2_o  => cpu_iwb_in);
 
@@ -194,8 +265,8 @@ begin  -- rtl
     port map (
       clk_sys_i  => clk_sys_i,
       rst_n_i    => rst_n_i,
-      slave_i    => cnx_master_out(1),
-      slave_o    => cnx_master_in(1),
+      slave_i    => cnx_master_out(c_SLAVE_UART),
+      slave_o    => cnx_master_in(c_SLAVE_UART),
       desc_o     => open,
       uart_rxd_i => uart_rxd_i,
       uart_txd_o => uart_txd_o);
@@ -203,16 +274,26 @@ begin  -- rtl
 
   U_SoftPLL : xwr_softpll_ng
     generic map (
-      g_tag_bits                 => 22,
-      g_interface_mode           => PIPELINED,
-      g_address_granularity      => BYTE,
-      g_num_ref_inputs           => g_num_rx_clocks + 2,
-      g_num_outputs              => 1)
+      g_tag_bits             => 22,
+      g_interface_mode       => PIPELINED,
+      g_address_granularity  => BYTE,
+      g_num_ref_inputs       => g_num_rx_clocks,
+      g_num_outputs          => 1,
+      g_reverse_dmtds        => false,
+      g_with_ext_clock_input => true,
+      g_with_period_detector => false,
+      g_with_undersampling   => false,
+      g_with_debug_fifo      => true,
+      g_bb_ref_divider       => 8,
+      g_bb_feedback_divider  => 25,
+      g_bb_log2_gating       => 13)
     port map (
       clk_sys_i       => clk_sys_i,
       rst_n_i         => rst_n_i,
       clk_ref_i       => clk_rx_vec,
       clk_fb_i(0)     => clk_ref_i,
+      clk_ext_i       => clk_ext_i,
+      sync_p_i        => pps_ext_i,
       clk_dmtd_i      => clk_dmtd_i,
       dac_dmtd_data_o => dac_dmtd_data,
       dac_dmtd_load_o => dac_dmtd_load,
@@ -221,9 +302,33 @@ begin  -- rtl
       dac_out_load_o  => dac_out_load,
       out_enable_i    => "0",
       out_locked_o    => open,
-      slave_i         => cnx_master_out(2),
-      slave_o         => cnx_master_in(2),
+      slave_i         => cnx_master_out(c_SLAVE_SOFTPLL),
+      slave_o         => cnx_master_in(c_SLAVE_SOFTPLL),
       debug_o         => open);
+
+  
+  U_PPS_Gen : xwr_pps_gen
+    generic map (
+      g_interface_mode       => PIPELINED,
+      g_address_granularity  => BYTE,
+      g_ref_clock_rate       => f_pick(g_simulation, 10000, 62500000),
+      g_ext_clock_rate       => f_pick(g_simulation, 1600, 10000000),
+      g_with_ext_clock_input => true)
+    port map (
+      clk_ref_i       => clk_ref_i,
+      clk_sys_i       => clk_sys_i,
+      clk_ext_i       => clk_ext_i,
+      rst_n_i         => gpio_out(3),
+      slave_i         => cnx_master_out(c_SLAVE_PPSGEN),
+      slave_o         => cnx_master_in(c_SLAVE_PPSGEN),
+      pps_in_i        => pps_ext_i,
+      pps_csync_o     => pps_csync_o,
+      pps_out_o       => pps_ext_o,
+      pps_valid_o     => pps_valid_o,
+      tm_utc_o        => open,
+      tm_cycles_o     => open,
+      tm_time_valid_o => open);
+
 
   cpu_irq_vec(0)           <= cnx_master_in(2).int;
   cpu_irq_vec(31 downto 1) <= (others => '0');
@@ -235,8 +340,8 @@ begin  -- rtl
     port map (
       clk_sys_i            => clk_sys_i,
       rst_n_i              => rst_n_i,
-      slave_i              => cnx_master_out(3),
-      slave_o              => cnx_master_in(3),
+      slave_i              => cnx_master_out(c_SLAVE_SPI),
+      slave_o              => cnx_master_in(c_SLAVE_SPI),
       desc_o               => open,
       pad_cs_o(0)          => pll_cs_n_o,
       pad_cs_o(7 downto 1) => dummy(7 downto 1),
@@ -253,8 +358,8 @@ begin  -- rtl
     port map (
       clk_sys_i  => clk_sys_i,
       rst_n_i    => rst_n_i,
-      slave_i    => cnx_master_out(4),
-      slave_o    => cnx_master_in(4),
+      slave_i    => cnx_master_out(c_SLAVE_GPIO),
+      slave_o    => cnx_master_in(c_SLAVE_GPIO),
       desc_o     => open,
       gpio_b     => open,
       gpio_out_o => gpio_out,
@@ -271,8 +376,8 @@ begin  -- rtl
     port map (
       clk_sys_i => clk_sys_i,
       rst_n_i   => rst_n_i,
-      slave_i   => cnx_master_out(5),
-      slave_o   => cnx_master_in(5),
+      slave_i   => cnx_master_out(c_SLAVE_TIMER),
+      slave_o   => cnx_master_in(c_SLAVE_TIMER),
       desc_o    => open);
 
   sel_clk_sys_o <= gpio_out(0);
