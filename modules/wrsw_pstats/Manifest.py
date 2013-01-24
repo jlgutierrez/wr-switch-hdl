d724022bfce891abeca568dcc5dba2213ae9b923
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-01-24 11:08:48 +0100

    greg_counters: add record-based wrapper for wrsw_pstats

--- modules/wrsw_pstats/Manifest.py
+++ modules/wrsw_pstats/Manifest.py
@@ -1,1 +1,1 @@
-files = ["wrsw_pstats.vhd", "port_cntr.vhd", "pstats_wishbone_slave.vhd", "pstats_wbgen2_pkg.vhd", "dummy_rmon.vhd"]
+files = ["wrsw_pstats.vhd", "port_cntr.vhd", "pstats_wishbone_slave.vhd", "pstats_wbgen2_pkg.vhd", "xwrsw_pstats.vhd", "dummy_rmon.vhd"]
