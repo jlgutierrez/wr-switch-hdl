4c81052baec86606e6b2cbc6038399c9a52fd147
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-02-18 10:34:40 +0100

    greg_counters: move dummy_rmon to a separate subdirectory, it's here only for veryfing  wrsw_pstats

--- modules/wrsw_pstats/Manifest.py
+++ modules/wrsw_pstats/Manifest.py
@@ -1,1 +1,2 @@
-files = ["wrsw_pstats.vhd", "port_cntr.vhd", "pstats_wishbone_slave.vhd", "pstats_wbgen2_pkg.vhd", "xwrsw_pstats.vhd", "dummy_rmon.vhd"]
+files = ["wrsw_pstats.vhd", "port_cntr.vhd", "pstats_wishbone_slave.vhd", "pstats_wbgen2_pkg.vhd", "xwrsw_pstats.vhd",
+    "wrsw_dummy"]
