c736a59a70d8a4ccb046bbfe4e86391202174e1a
Maciej Lipinski <maciej.lipinski@cern.ch>
2013-03-15 17:12:21 +0100

    SYN: confgured top sythesis files to sythesize all features (TRU+TATSU)

--- top/scb_8ports/scb_top_synthesis.vhd
+++ top/scb_8ports/scb_top_synthesis.vhd
@@ -229,7 +229,8 @@ architecture Behavioral of scb_top_synthesis is
       g_num_ports       : integer;
       g_simulation      : boolean;
       g_without_network : boolean;
-      g_with_TRU        : boolean);
+      g_with_TRU        : boolean;
+      g_with_TATSU      : boolean);
     port (
       sys_rst_n_i         : in  std_logic;
       clk_startup_i       : in  std_logic;
@@ -551,7 +552,8 @@ begin
       g_num_ports       => c_NUM_PORTS,
       g_simulation      => g_simulation,
       g_without_network => false,
-      g_with_TRU        => true)
+      g_with_TRU        => true,
+      g_with_TATSU      => true)
     port map (
       sys_rst_n_i         => sys_rst_n_i,
       clk_startup_i       => clk_sys_startup,
