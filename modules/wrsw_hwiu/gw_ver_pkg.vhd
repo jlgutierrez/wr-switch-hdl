e6aae2bc62697fbc2736fad599d706d6416aa5fd
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-07-18 13:39:23 +0200

    hwiu: update autogenerated gw_ver package

--- modules/wrsw_hwiu/gw_ver_pkg.vhd
+++ modules/wrsw_hwiu/gw_ver_pkg.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 --generated automatically by gen_ver.py script--
 package hwver_pkg is
-constant c_build_date : std_logic_vector(31 downto 0) := x"11070d00";
-constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"0aced65b";
+constant c_build_date : std_logic_vector(31 downto 0) := x"12070d00";
+constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"05dec283";
 constant c_gencores_ver : std_logic_vector(31 downto 0) := x"04beed17";
 constant c_wrcores_ver : std_logic_vector(31 downto 0) := x"00aafd08";
 end package;
