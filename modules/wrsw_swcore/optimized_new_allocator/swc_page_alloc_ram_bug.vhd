433d80bb26eceeb742e19f957222aea56e6d7c90
Maciej Lipinski <maciej.lipinski@cern.ch>
2013-10-15 19:00:28 +0200

    [swcore/alloc] new allocator adapter to the bug-less RAM - tested for single-port

--- modules/wrsw_swcore/optimized_new_allocator/swc_page_alloc_ram_bug.vhd
+++ modules/wrsw_swcore/optimized_new_allocator/swc_page_alloc_ram_bug.vhd
@@ -50,8 +50,7 @@
 -- 2012-01-24  2.0      twlostow completely changed (uses FIFO)
 -- 2012-03-05  2.1      mlipinsk added debugging stuff + made interchangeable with old (still buggy)
 -- 2012-03-15  2.2      twlostow fixed really ugly missing pages bug
--- 2012-10-11  3.0      mlipinsk optimized to work in single cycle + pipelined
--- 2012-10-15  3.0      mlipinsk changed to bug-free-ram
+-- 2012-10-11  3.1      mlipinsk optimized to work in single cycle + pipelined
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -154,6 +153,7 @@ architecture syn of swc_page_allocator_new is
 
   signal q_write_p1     : std_logic;
   signal q_read_p0      : std_logic;
+  signal q_read_d1      : std_logic;
 
   signal initializing : std_logic;
 
@@ -162,6 +162,7 @@ architecture syn of swc_page_allocator_new is
   signal usecnt_rddata_p1, usecnt_data_wr_p1 : std_logic_vector(g_usecount_width-1 downto 0);
 
   signal q_output_addr_p1 : std_logic_vector(g_page_addr_width-1 downto 0);
+  signal q_input_addr_p1  : std_logic_vector(g_page_addr_width-1 downto 0);
   signal done_p1      : std_logic;
   signal ram_ones      : std_logic_vector(g_page_addr_width + g_usecount_width -1 downto 0);
 
@@ -226,89 +227,84 @@ begin  -- syn
   
   -- write queue when freeing and not initializing (because when initializing we use other
   -- port of this memory)
-  q_write_p1 <= (not initializing) when
-                (alloc_req_d1.free   = '1' and unsigned(usecnt_rddata_p1) = 1) or
-                (alloc_req_d1.f_free = '1') else '0';
+  q_write_p1 <= '1' when (alloc_req_d1.free   = '1' and unsigned(usecnt_rddata_p1) = 1) or
+                         (alloc_req_d1.f_free = '1') else initializing;
 
   -- increaze pointer to next address -> it stores next freee page, we use the currently read
   -- and increase for next usage
-  q_read_p0 <= '1' when (alloc_req_d0.alloc = '1') else '0';-- and (real_nomem_d0 = '0') else '0'; -- TODO: real_nomem
-
-  U_Queue_RAM : generic_dpram
-    generic map (
-      g_data_width               => g_page_addr_width,
-      g_size                     => 2**g_page_addr_width,
-      g_with_byte_enable         => false,
-      g_dual_clock               => false)
-    port map (
-      rst_n_i => rst_n_i,
-      clka_i  => clk_i,
-      bwea_i  => ram_ones((g_page_addr_width+7)/8 - 1 downto 0),
-      wea_i   => q_write_p1,
-      aa_i    => std_logic_vector(wr_ptr_p1), -- pointer increase on each q_write_p1=HIGH
-      da_i    => alloc_req_d1.pgaddr,
-
-      clkb_i => clk_i,
-      bweb_i => ram_ones((g_page_addr_width+7)/8 - 1 downto 0),
-      web_i  => initializing,
-      ab_i   => std_logic_vector(rd_ptr_p0), -- pointer increase on each q_read_p0=HIGH
-      db_i   => std_logic_vector(rd_ptr_p0), -- pointer increase on each q_read_p0=HIGH
-      qb_o   => q_output_addr_p1);
-
+  q_read_p0  <= '1' when (alloc_req_d0.alloc = '1') else '0';-- and (real_nomem_d0 = '0') else '0'; -- TODO: real_nomem
   
-  usecnt_addr_wr_p1 <= q_output_addr_p1 when alloc_req_d1.alloc = '1' else alloc_req_d1.pgaddr;
+  -- address of page stored in the memory (queue)
+  q_input_addr_p1 <= std_logic_vector(wr_ptr_p1) when initializing = '1' else alloc_req_d1.pgaddr;
 
-  usecnt_ena_wr_p1  <= (alloc_req_d1.alloc         or 
+  -- memeory in which list of addresses of available pages is stored
+  -- rd_ptr points to the address of next available page_addr
+  -- wr_ptr points to the address where a freed page can be written
+  U_Queue_RAM : swc_rd_wr_ram
+    generic map (
+      g_data_width => g_page_addr_width,
+      g_size       => 2**g_page_addr_width)
+    port map (
+      clk_i   => clk_i,
+      rst_n_i => rst_n_i,
+      we_i    => q_write_p1,
+      wa_i    => std_logic_vector(wr_ptr_p1),
+      wd_i    => q_input_addr_p1,
+      ra_i    => std_logic_vector(rd_ptr_p0),
+      rd_o    => q_output_addr_p1);
+ 
+  usecnt_addr_wr_p1 <= std_logic_vector(rd_ptr_p0) when initializing       = '1' else
+                       q_output_addr_p1            when alloc_req_d1.alloc = '1' else 
+                       alloc_req_d1.pgaddr;
+
+  usecnt_ena_wr_p1  <=  alloc_req_d1.alloc         or 
                         alloc_req_d1.set_usecnt    or 
                         alloc_req_d1.free          or 
-                        alloc_req_d1.f_free)      and not initializing;
+                        alloc_req_d1.f_free        or
+                        initializing;
 
   usecnt_data_wr_p1 <= alloc_req_d1.usecnt                    when alloc_req_d1.set_usecnt = '1' else 
                        alloc_req_d1.usecnt                    when alloc_req_d1.alloc      = '1' else
                        f_gen_dummy_vec('0', g_usecount_width) when alloc_req_d1.f_free     = '1' else
+                       f_gen_dummy_vec('0', g_usecount_width) when initializing            = '1' else
                        std_logic_vector(unsigned(usecnt_rddata_p1) - 1);
 
-  usecnt_addr_rd_p0<= std_logic_vector(rd_ptr_p0) when initializing = '1' else alloc_req_d0.pgaddr;
+  usecnt_addr_rd_p0<= alloc_req_d0.pgaddr;
 
-  U_UseCnt_RAM : generic_dpram
+  -- stores usecnts of pages, the addres is the page_addr (not ptr)
+  U_UseCnt_RAM : swc_rd_wr_ram
     generic map (
-      g_data_width       => g_usecount_width,
-      g_size             => 2**g_page_addr_width,
-      g_with_byte_enable => false,
-      g_dual_clock       => false)
+      g_data_width => g_usecount_width,
+      g_size       => 2**g_page_addr_width)
     port map (
+      clk_i   => clk_i,
       rst_n_i => rst_n_i,
-      clka_i  => clk_i,
-      wea_i   => usecnt_ena_wr_p1,
-      bwea_i  => ram_ones((g_usecount_width+7)/8 - 1 downto 0),
-      aa_i    => usecnt_addr_wr_p1,
-      da_i    => usecnt_data_wr_p1,
-
-      clkb_i  => clk_i,
-      bweb_i  => ram_ones((g_usecount_width+7)/8 - 1 downto 0),
-      web_i   => initializing,
-      ab_i    => usecnt_addr_rd_p0,--std_logic_vector(rd_ptr_p0),
-      db_i    => f_gen_dummy_vec('0', g_usecount_width),
-      qb_o    => usecnt_rddata_p1);
+      we_i    => usecnt_ena_wr_p1,
+      wa_i    => usecnt_addr_wr_p1,
+      wd_i    => usecnt_data_wr_p1,
+      ra_i    => usecnt_addr_rd_p0,
+      rd_o    => usecnt_rddata_p1);
 
   p_pointers : process(clk_i)
   begin
     if rising_edge(clk_i) then
       if rst_n_i = '0' then
-        initializing <= '1';
-        rd_ptr_p0       <= (others => '0');
-        real_nomem   <= '0';
+        initializing     <= '1';
+        rd_ptr_p0        <= (others => '0');
+        wr_ptr_p1        <= (others => '0');
+        real_nomem       <= '0';
+        free_pages       <= to_unsigned(g_num_pages-1, free_pages'length);
+        q_read_d1        <= '0';
       else
-
-        real_nomem_d0 <= real_nomem;
+        q_read_d1        <= q_read_p0;
+        real_nomem_d0    <= real_nomem;
 
         if(initializing = '1') then
-          free_pages <= to_unsigned(g_num_pages-1, free_pages'length);
-          wr_ptr_p1     <= to_unsigned(g_num_pages-1, wr_ptr_p1'length);
-          rd_ptr_p0     <= rd_ptr_p0 + 1;
-          if(rd_ptr_p0 = g_num_pages-2) then
+          if(wr_ptr_p1 = g_num_pages-1) then
             initializing <= '0';
-            rd_ptr_p0       <= (others => '0');
+            wr_ptr_p1    <= to_unsigned(g_num_pages-1, wr_ptr_p1'length);
+          else
+            wr_ptr_p1    <= wr_ptr_p1 + 1;
           end if;
         else
           -- just increaing the pointerst to end/beginning of the queue
@@ -370,7 +366,7 @@ begin  -- syn
   pgaddr_o           <= q_output_addr_p1;
   done_o             <= done_p1;                 
   rsp_vec_o          <= alloc_req_d1.grant_vec;
-  nomem_o            <= out_nomem;
+  nomem_o            <= out_nomem or initializing;
   free_last_usecnt_o <= (not initializing) when (alloc_req_d1.free = '1' and unsigned(usecnt_rddata_p1) = 1) else '0';
 
 end syn;
