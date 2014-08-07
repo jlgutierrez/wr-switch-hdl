dc822248a71ba82dc1c29feeff5949bc3e546c3f
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2014-08-07 11:46:33 +0200

    hwiu gateware info for v4.0 release

--- modules/wrsw_hwiu/gw_ver_pkg.vhd
+++ modules/wrsw_hwiu/gw_ver_pkg.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 --generated automatically by gen_ver.py script--
 package hwver_pkg is
-constant c_build_date : std_logic_vector(31 downto 0) := x"12060e00";
-constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"0508ae9f";
-constant c_gencores_ver : std_logic_vector(31 downto 0) := x"05118070";
-constant c_wrcores_ver : std_logic_vector(31 downto 0) := x"07efeb16";
+constant c_build_date : std_logic_vector(31 downto 0) := x"04080e00";
+constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"0cc6e771";
+constant c_gencores_ver : std_logic_vector(31 downto 0) := x"0ae5ff9a";
+constant c_wrcores_ver : std_logic_vector(31 downto 0) := x"0de3d197";
 end package;
