09e4d489da828449c8fb82a149a935136e6c60f7
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2015-07-14 11:38:34 +0200

    hwiu: update info for v4.2

--- modules/wrsw_hwiu/gw_ver_pkg.vhd
+++ modules/wrsw_hwiu/gw_ver_pkg.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 --generated automatically by gen_ver.py script--
 package hwver_pkg is
-constant c_build_date : std_logic_vector(31 downto 0) := x"0d070f00";
-constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"003b3a81";
+constant c_build_date : std_logic_vector(31 downto 0) := x"0e070f00";
+constant c_switch_hdl_ver : std_logic_vector(31 downto 0) := x"081f37c2";
 constant c_gencores_ver : std_logic_vector(31 downto 0) := x"012c045e";
-constant c_wrcores_ver : std_logic_vector(31 downto 0) := x"0477ea63";
+constant c_wrcores_ver : std_logic_vector(31 downto 0) := x"004583a9";
 end package;
